eb4cc0d0fec93157dfdaa7b38c762d0f63074c9f
quezia <quezia.emanuelly2003@gmail.com>
2026-01-10 20:25:55 -0300

    adicionando mais casos de teste na validação de cpf

--- src/test/java/unit/UserTest.java
+++ src/test/java/unit/UserTest.java
@@ -13,6 +13,8 @@ public class UserTest {
     @Test
     public void validarCpfTest()
     {
-        assertThrows(IllegalArgumentException.class, () -> User.validarCPF("1234567898"));
+        assertThrows(IllegalArgumentException.class, () -> User.validarCPF("123"));
+        assertThrows(IllegalArgumentException.class, () -> User.validarCPF("12345678912"));
+        assertDoesNotThrow(() -> User.validarCPF("37107107070"));
     }
 }
