840e4fbeede93ac47ed160abfa1a28b3b3809f22
quezia <quezia.emanuelly2003@gmail.com>
2026-01-15 16:29:48 -0300

    arrumando funções das telas para ficar em tela cheia

--- src/main/java/org/trab/demo/util/Telas.java
+++ src/main/java/org/trab/demo/util/Telas.java
@@ -1,6 +1,7 @@
 package org.trab.demo.util;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,18 +10,21 @@ import java.io.IOException;
 public class Telas {
 
     private static Stage stageC;
-    private static int width = 900;
-    private static int height = 700;
     public static void getTelaLogin(Stage stage) throws IOException
     {
-        if (stage != null) {
-            stageC = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(Telas.class.getResource("/org/trab/demo/login.fxml"));
+        if (stage == null) {
+            Parent root = fxmlLoader.load();
+            stageC.getScene().setRoot(root);
+            return;
         }
 
-        FXMLLoader fxmlLoader = new FXMLLoader(Telas.class.getResource("/org/trab/demo/login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), width, height);
+        stageC = stage;
+        Scene scene = new Scene(fxmlLoader.load());
 
         stageC.setScene(scene);
+        stageC.setMaximized(false);
+        stageC.setMaximized(true);
         stageC.show();
     }
         public static Stage getStage() {
@@ -30,85 +34,84 @@ public class Telas {
     public static void getTelaCadastro() throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(Telas.class.getResource("/org/trab/demo/cadastro.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), width, height);
+        Parent root = fxmlLoader.load();
 
-        stageC.setScene(scene);
+        stageC.getScene().setRoot(root);
         stageC.show();
     }
 
     public static void getTelaDashPsi() throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(Telas.class.getResource("/org/trab/demo/dash_adm.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), width, height);
-
-        stageC.setScene(scene);
+        Parent root = fxmlLoader.load();
+        stageC.getScene().setRoot(root);
         stageC.show();
     }
 
     public static void getTelaDashPaci() throws IOException
    {
         FXMLLoader fxmlLoader = new FXMLLoader(Telas.class.getResource("/org/trab/demo/dashPaciente.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), width, height);
-        stageC.setScene(scene);
+        Parent root = fxmlLoader.load();
+        stageC.getScene().setRoot(root);
         stageC.show();
     }
 
     public static void getTelaEditPsi() throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(Telas.class.getResource("/org/trab/demo/edit_psicologo.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), width, height);
-        stageC.setScene(scene);
+        Parent root = fxmlLoader.load();
+        stageC.getScene().setRoot(root);
         stageC.show();
     }
 
     public static void getTelaCadHorarios() throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(Telas.class.getResource("/org/trab/demo/cadastra_horario.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), width, height);
-        stageC.setScene(scene);
+        Parent root = fxmlLoader.load();
+        stageC.getScene().setRoot(root);
         stageC.show();
     }
 
     public static void getTelaAgendaPsicologo() throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(Telas.class.getResource("/org/trab/demo/agenda.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), width, height);
-        stageC.setScene(scene);
+        Parent root = fxmlLoader.load();
+        stageC.getScene().setRoot(root);
         stageC.show();
     }
     public static void getTelaHistoricoConsultas() throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(Telas.class.getResource("/org/trab/demo/historico_consultas.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), width, height);
-        stageC.setScene(scene);
+        Parent root = fxmlLoader.load();
+        stageC.getScene().setRoot(root);
         stageC.show();
     }
 
     public static void getTelaPerfil() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Telas.class.getResource("/org/trab/demo/dashPerfilPaciente.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), width, height);
-        stageC.setScene(scene);
+        Parent root = fxmlLoader.load();
+        stageC.getScene().setRoot(root);
         stageC.show();
     }
 
     public static void getTelaAgendamento() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Telas.class.getResource("/org/trab/demo/dashAgendamento.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), width, height);
-        stageC.setScene(scene);
+        Parent root = fxmlLoader.load();
+        stageC.getScene().setRoot(root);
         stageC.show();
     }
 
     public static void getTelaRemarcacao() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Telas.class.getResource("/org/trab/demo/dashRemarcação.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), width, height);
-        stageC.setScene(scene);
+        Parent root = fxmlLoader.load();
+        stageC.getScene().setRoot(root);
         stageC.show();
     }
 
     public static void getTelaCancelamento() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Telas.class.getResource("/org/trab/demo/dashCancelamento.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), width, height);
-        stageC.setScene(scene);
+        Parent root = fxmlLoader.load();
+        stageC.getScene().setRoot(root);
         stageC.show();
     }
 
